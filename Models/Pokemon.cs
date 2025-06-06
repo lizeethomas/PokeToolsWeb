ecd3afc16ba74181e4352954766d24b839f30ea3
psycox <lizee.etu@gmail.com>
2025-06-07 01:23:09 +0200

    v1

---
+++ Models/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿namespace PokéToolsWeb.Models
+{
+    public class Pokemon
+    {
+        public int Dex { get; set; }
+        public string Name { get; set; }
+        public string Form { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+        public int Total { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpAtk { get; set; }
+        public int SpDef { get; set; }
+        public int Speed { get; set; }
+        public int Gen { get; set; }
+
+        public string? Nom { get; set; }
+        public int? Ether { get; set; }
+        public int? Psycox { get; set; }
+
+        public string IconUrl { get; set; }
+
+    }
+}
