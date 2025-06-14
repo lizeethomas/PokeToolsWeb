ee2a7af66b1a93443b1db0b932e7a69fdaf1a22c
psycox <lizee.etu@gmail.com>
2025-06-15 01:27:46 +0200

    v2 interface beboule help

---
+++ Models/DefensiveTypeChart.cs
@@ -0,0 +1,11 @@
+﻿namespace PokéToolsWeb.Models
+{
+    public class DefensiveTypeChart
+    {
+        public string Pokemon { get; set; }
+        public string[] Types { get; set; }
+        public Dictionary<string, double> Weaknesses { get; set; }
+        public Dictionary<string, double> Resistances { get; set; }
+        public Dictionary<string, double> Immunities { get; set; }
+    }
+}
